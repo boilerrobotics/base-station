deafa9219b0ebf44ce69ff9fc0c2f83102d0ab3a
tame0001 <thirawat.tam@gmail.com>
2023-10-28 20:13:43 -0400

    create remote operation package

---
+++ src/remote_operation/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'remote_operation'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Tam Bureetes',
+    maintainer_email='tbureete@purdue.edu',
+    description='Remote operation package on base station',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
