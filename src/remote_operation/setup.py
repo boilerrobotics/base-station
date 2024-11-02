7ff7b9822d3525f4b2277d54243631d4965be934
tame0001 <thirawat.tam@gmail.com>
2024-11-02 12:41:08 -0400

    build remote operation launch file

--- src/remote_operation/setup.py
+++ src/remote_operation/setup.py
@@ -1,25 +1,30 @@
+import os
+from glob import glob
 from setuptools import find_packages, setup
 
-package_name = 'remote_operation'
+package_name = "remote_operation"
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=['test']),
+    version="0.0.1",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        # Include all launch files
+        (
+            os.path.join("share", package_name, "launch"),
+            glob("launch/*launch.[pxy][yma]*"),
+        ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Tam Bureetes',
-    maintainer_email='tbureete@purdue.edu',
-    description='Remote operation package on base station',
-    license='MIT',
-    tests_require=['pytest'],
+    maintainer="Tam Bureetes",
+    maintainer_email="tbureete@purdue.edu",
+    description="Remote operation package on base station",
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-        ],
+        "console_scripts": [],
     },
 )
