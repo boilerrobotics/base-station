3a2cfa48cb5e6227d4eec3b863b560020df76c91
zackhom <zhomrich@purdue.edu>
2025-02-01 13:46:49 -0500

    committing

--- src/joystick/joystick/control_rover.py
+++ src/joystick/joystick/control_rover.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default
+from rclpy.qos import QoSProfile,HistoryPolicy, DurabilityPolicy, Duration, LivelinessPolicy, ReliabilityPolicy
 
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
@@ -10,13 +11,21 @@ class RoverControlPublisherNode(Node):
 
     def __init__(self):
         super().__init__("rover_manual_control_publisher")
+        qos_profile = QoSProfile(
+            history = HistoryPolicy.KEEP_LAST,
+            reliability = ReliabilityPolicy.BEST_EFFORT,
+            depth = 5,
+            durability = DurabilityPolicy.VOLATILE,
+            liveliness = LivelinessPolicy.AUTOMATIC,
+            lifespan = Duration(seconds=.1, nanoseconds=0)
+        )
         self.subscription = self.create_subscription(
-            Joy, "joy", self.controller_callback, qos_profile_sensor_data
+            Joy, "joy", self.controller_callback, qos_profile
         )
         self.publisher_ = self.create_publisher(
             Twist,
             "cmd_vel",
-            qos_profile_system_default,
+            qos_profile_sensor_data,
             # Might need to modify QOS policy.
             # Better to use sensor data but it is not compatible with Gazebo policy
             # It could be fixed by changing gazebo policy
